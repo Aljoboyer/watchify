f6b3881ed2930e56be3ccfd44de6837c62ceec84
Aljoboyer <tanveeazmain@gmail.com>
2025-08-24 15:49:24 +0600

    be server deployed

--- watchify-be/index.js
+++ watchify-be/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 require("dotenv").config();
-const port = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors())
 app.use(express.json()); // For JSON bodies
@@ -29,8 +29,8 @@ app.get('/', (req, res) => {
   res.send('Watchify Server is connected!')
 })
 
-app.listen(port, () => {
-  console.log(`Watchify Server running on port ${port}`);
+app.listen(PORT, () => {
+  console.log(`Watchify Server running on port ${PORT}`);
 });
 
 // module.exports = app;
